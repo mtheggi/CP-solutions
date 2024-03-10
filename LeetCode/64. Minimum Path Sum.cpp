477534b8caa766772a56413e7dcc74a162e8d337
Mohamed Heggi <mtheggi@gmail.com>
2024-03-10 17:28:34 +0200

    10/3

---
+++ LeetCode/64. Minimum Path Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int  m= grid.size() , n= grid[0].size() ; 
+
+        vector<vector<int>> dp(m ,vector<int>(n , 0 )); 
+        dp[0][0]=grid[0][0]; 
+        for(int i =0 ;i < m; i++) {
+            for(int j =0 ; j<n ; j++) {
+                if(i==0 && j ==0 ) continue;  
+                int left = (i-1)>=0 ? dp[i-1][j] : INT_MAX ;   
+                int up =(j-1)>=0 ? dp[i][j-1] : INT_MAX; 
+                dp[i][j] = min( left , up) + grid[i][j]; 
+            }
+        } 
+        return dp[m-1][n-1]; 
+    }
+};
