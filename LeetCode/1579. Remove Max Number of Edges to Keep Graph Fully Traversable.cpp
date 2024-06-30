970c756cd60c8a146bfbcab8922db807ece2482d
Mohamed Heggi <mtheggi@gmail.com>
2024-06-30 16:43:32 +0300

    30/6

---
+++ LeetCode/1579. Remove Max Number of Edges to Keep Graph Fully Traversable.cpp
@@ -0,0 +1,118 @@
+class Solution {
+    int size;
+    int component;
+    int *sz;
+    int *parent;
+public:
+    void construct(int size_ ) {
+        size = size_; 
+        component=size_; 
+        sz = new int[size_]; 
+        parent = new int[size_]; 
+        for (int i = 0; i < size_; i++) {
+            sz[i] = 1;
+            parent[i] = i;
+        }
+    }
+    int Find(int a){
+        if (parent[a] == a) return a;
+        int root = a;
+        while (root != parent[root]) {
+            root = parent[root];
+        }
+        while (a != root) {
+            int temp = parent[a];
+            parent[a] = root;
+            a = temp;
+        }
+        return root; 
+    }
+    int getcomponent() { return component; }
+    bool connected(int a, int p) { return (Find(a) == Find(p)); }
+    void unify(int a, int b) { // unify by size 
+       int root_a = Find(a);
+       int root_b = Find(b);
+       if (root_a == root_b) return; // cycles --> they are connected 
+       if (sz[root_a] < sz[root_b]) swap(root_a, root_b);
+       parent[root_b] = root_a;
+       sz[root_a] += sz[root_b];
+       component--;
+    }
+
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {        
+        construct(n); 
+        int ans =0 ; 
+        map<pair<int,int>, vector<int>> newEdges;  
+        map<vector<int>,bool> removed;  
+        for(auto edge : edges ) newEdges[make_pair(min(edge[1],edge[2])  , max(edge[1],edge[2]) )].push_back(edge[0]); 
+        for(auto  &edge: newEdges) {
+            if(find(edge.second.begin() , edge.second.end() , 3) != edge.second.end()){
+                ans+=(edge.second.size()-1);
+                edge.second = { 3 }; 
+            }
+        }
+        // alice 
+        for(auto edge : newEdges) {
+            if(find(edge.second.begin() , edge.second.end() , 3) != edge.second.end()){
+                int a = edge.first.first  , b = edge.first.second; 
+                a--; 
+                b--;
+                if(connected(a , b )){
+                    ans++; 
+                    removed[{3 , a ,b}]=true; 
+                }else{
+                    unify(a,b); 
+                }
+            } 
+        }
+        for(auto edge : newEdges) {
+            if(find(edge.second.begin() , edge.second.end() , 1) != edge.second.end()){
+                int a = edge.first.first  , b = edge.first.second; 
+                a--; 
+                b--;
+                if(connected(a , b )){
+                    ans++; 
+                    removed[{1 , a ,b}]=true; 
+                }else{
+                    unify(a,b); 
+                }
+
+            }
+        }
+        if(getcomponent() != 1 ) return -1 ; 
+        construct(n); 
+        // bob 
+        for(auto edge : newEdges) {
+            if(find(edge.second.begin() , edge.second.end() , 3) != edge.second.end()){
+                int a = edge.first.first  , b = edge.first.second; 
+                a--; 
+                b--;
+                if(!connected(a , b )){
+                    unify(a,b); 
+                }
+            } 
+        }
+        for(auto edge : newEdges) {
+            if(find(edge.second.begin() , edge.second.end() , 2) != edge.second.end()){
+                int a = edge.first.first  , b = edge.first.second; 
+                a--; 
+                b--;
+                if(connected(a , b )){
+                    ans++; 
+                    removed[{2 , a ,b}]=true; 
+                }else{
+                    unify(a,b); 
+                }
+
+            }
+        }
+        if(getcomponent() != 1 ) return -1 ; 
+    
+        return ans;
+    }
+
+    ~Solution(){
+        delete[] sz;
+        delete[] parent;
+    }
+};
